76d0f4b7fcae1bad84c511e4d620807036254f59
sijanz <simon.janzon@gmx.net>
2025-10-27 09:26:48 +0000

    renamed node

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'semantic_segmentation_node = scene_graph_room_classification.semantic_segmentation.semantic_segmentation_node:main',
+            'image_segmentation_node = scene_graph_room_classification.image_segmentation.image_segmentation_node:main',
             '3d_object_localization_node = scene_graph_room_classification.3d_object_localization.3d_object_localization_node:main',
             'scene_graph_fusion_node = scene_graph_room_classification.scene_graph_fusion.scene_graph_fusion_node:main'
         ],
